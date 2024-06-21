1135a3b8ff79f477a519659893bf5dc1e4a8a90d
giser_liyong <dixin_liyong@163.com>
2024-06-21 13:58:44 +0800

    fix:去除无用依赖

--- src/main/java/com/yong/controller/QueryController.java
+++ src/main/java/com/yong/controller/QueryController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.metamodel.ListAttribute;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
